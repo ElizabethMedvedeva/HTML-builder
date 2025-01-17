0a9e949d877e9241b1abcd5b572cdac549fc6547
ElizabethMedvedeva <elizabeth.medvedeva12@gmail.com>
2025-01-17 23:41:49 +0200

    feat:add solved 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'text.txt');
+
+const readStream = fs.createReadStream(filePath, { encoding: 'utf-8' });
+
+readStream.pipe(process.stdout);
+
+readStream.on('error', (error) => {
+  console.log('Error', error.message);
+});
